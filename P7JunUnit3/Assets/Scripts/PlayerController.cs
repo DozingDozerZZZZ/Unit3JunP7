c73fc48f2936a2c293d88aa67080c6b464f5e4fd
DozingDozerZZZZ <763664@student.dusd.net>
2026-04-01 15:18:55 -0700

    unit 3 done

--- P7JunUnit3/Assets/Scripts/PlayerController.cs
+++ P7JunUnit3/Assets/Scripts/PlayerController.cs
@@ -12,14 +12,24 @@ public class PlayerController : MonoBehaviour
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
 
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+
+    public bool doubleSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
-       Physics.gravity *= gravityModifer;
+        Physics.gravity *= gravityModifer;
 
-        
+        playerAudio = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -31,26 +41,48 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("SpeedMultiplier", 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
             dirtParticle.Play();
         }
 
-        else if(collision.gameObject.CompareTag("Obstacle"))
+        else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game over..");
-            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
--- P7JunUnit3/Assets/Scripts/SpawnManager.cs
+++ P7JunUnit3/Assets/Scripts/SpawnManager.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3 (25,0,0);
 
     private float startDelay = 2;
     private float repeatRate = 2;
+    private int randomObstacle;
 
     private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,8 +28,10 @@ public class SpawnManager : MonoBehaviour
     {
         if(playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs [randomObstacle].transform.rotation);
         }
         
     }
+ 
 }
